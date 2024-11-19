57c84edba77c328d68684d61004585065de10c44
kant003 <gonzalezm.angel@gmail.com>
2024-11-19 14:34:00 +0100

    mover el jugador por la pantalla

--- src/pages/asteroidsGame/game.ts
+++ src/pages/asteroidsGame/game.ts
@@ -1,21 +1,35 @@
+import { Player } from "./player"
+export const WIDTH = 800
+export const HEIGHT = 600
+
 export default function initGame(){
     const canvas = document.getElementById('miCanvas') as HTMLCanvasElement
     const ctx = canvas.getContext('2d')
-    canvas.width = 800
-    canvas.height = 600
+    canvas.width = WIDTH
+    canvas.height = HEIGHT
     canvas.style.border = '1px solid white'
+
+    const player1 = new Player(WIDTH/2, HEIGHT/2)
+
     function gameLoop(){
         ctx?.clearRect(0,0 ,canvas.width, canvas.height)
+
+        player1.draw(ctx)
+        player1.update()
 
         requestAnimationFrame(gameLoop)
     }
     gameLoop()
 
     document.addEventListener('keydown', (e)=>{
-        
+        if(e.key === 'ArrowLeft') player1.isRotatingLeft = true
+        if(e.key === 'ArrowRight') player1.isRotatingRight = true
+        if(e.key === 'ArrowUp') player1.isThrusting = true
     })
 
     document.addEventListener('keyup', (e)=>{
-
+        if(e.key === 'ArrowLeft') player1.isRotatingLeft = false
+        if(e.key === 'ArrowRight') player1.isRotatingRight =false
+        if(e.key === 'ArrowUp') player1.isThrusting = false
     })
 }
