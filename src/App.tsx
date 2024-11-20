b1e051a3c3379acde6fe95e80db90c9133825ac1
andrespi97 <andreslage97@gmail.com>
2024-11-20 12:45:00 +0100

    treeview

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Ejer1 from './pages/ejer1'
-import Ejer2 from './pages/ejer2'
-import Layout from './pages/Layout'
-import NotFound from './pages/NotFound'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Ejer1 from "./pages/ejer1";
+import Ejer2 from "./pages/ejer2";
+import Layout from "./pages/Layout";
+import NotFound from "./pages/NotFound";
+import Treeview from "./pages/Treeview";
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout/>} >
-            <Route path="E1" element={<Ejer1/>} />
-            <Route path="E2" element={<Ejer2/>} />
-            <Route path="*" element={<NotFound/>} />
+        <Route path="/" element={<Layout />}>
+          <Route path="E1" element={<Ejer1 />} />
+          <Route path="E2" element={<Ejer2 />} />
+          <Route path="treeview" element={<Treeview />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
