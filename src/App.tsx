5b9e97e4a8cdae406e7f8db6b7652eb551bb8a58
javichuchio <javisio727@gmail.com>
2024-11-20 13:35:13 +0100

    swap

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import Ejer1 from './pages/ejer1'
-import Ejer2 from './pages/ejer2'
+import Ejer1 from './pages/Ejer1'
+import Ejer2 from './pages/Ejer2'
+import Ejer3 from './pages/Ejer3'
 import Layout from './pages/Layout'
 import NotFound from './pages/NotFound'
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Layout/>} >
             <Route path="E1" element={<Ejer1/>} />
             <Route path="E2" element={<Ejer2/>} />
+            <Route path="E3" element={<Ejer3/>} />
             <Route path="*" element={<NotFound/>} />
         </Route>
       </Routes>
