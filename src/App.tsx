50b8dd6f61e7f022e6accc9c5c96159ca94af23c
PabloCou <pablocourou@gmail.com>
2024-11-20 13:05:19 +0100

    Pagina del boton creada

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import Ejer1 from './pages/ejer1'
-import Ejer2 from './pages/ejer2'
+import Ejer1 from './pages/Ejer1'
+import Ejer2 from './pages/Ejer2'
 import Layout from './pages/Layout'
+import Boton from "./pages/Boton";
 import NotFound from './pages/NotFound'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Layout/>} >
             <Route path="E1" element={<Ejer1/>} />
             <Route path="E2" element={<Ejer2/>} />
+            <Route path="Boton" element={<Boton />} />
             <Route path="*" element={<NotFound/>} />
         </Route>
       </Routes>
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -8,11 +8,14 @@ function MainMenu(){
         <li>
             <Link to="/E2">Ejercicio2</Link>
         </li>
+        <li>
+            <Link to="/Boton">Boton</Link>
+        </li>
     </ul>
 </nav>
 }
 
-const Footer = () => <footer>Pagina creada por Angel</footer>
+const Footer = () => <footer>Pagina creada por Pablo</footer>
 
 function Layout(){
     return (
