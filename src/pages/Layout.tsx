eb672628fb1f66282e9b7d619351eed813971393
Anxo55 <anxo.campos260204@gmail.com>
2024-11-20 12:38:44 +0100

    scroll page

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -8,6 +8,9 @@ function MainMenu(){
         <li>
             <Link to="/E2">Ejercicio2</Link>
         </li>
+        <li>
+            <Link to="/ScrollPage">Scroll Page</Link>
+        </li>
     </ul>
 </nav>
 }
