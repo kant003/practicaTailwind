946f0d33bb5716a16e3086aa47d04673e7a31fde
kant003 <gonzalezm.angel@gmail.com>
2024-11-21 14:28:27 +0100

    refactorización

--- src/pages/uh38.tsx
+++ src/pages/uh38.tsx
@@ -2,7 +2,7 @@ function Uh38(){
     return (
         <div 
         className="radial-progress text-primary" 
-        style={{ '--value': 70 } as React.CSSProperties}
+        style={{ 'lue': 70 } as React.CSSProperties}
         role="progressbar" aria-label="Primary Radial Progressbar">70%</div>
     )
 }
