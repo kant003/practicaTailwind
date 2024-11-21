e919c02a527afe3039b00b40aed83952bea94e60
BraisFranRi <braisfranri@gmail.com>
2024-11-21 10:48:50 +0100

    User history 20 Brais FR

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,18 @@ import Ejer1 from './pages/ejer1'
 import Ejer2 from './pages/ejer2'
 import Layout from './pages/Layout'
 import NotFound from './pages/NotFound'
+import Uh20 from './pages/Uh20'
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout/>} >
-            <Route path="E1" element={<Ejer1/>} />
-            <Route path="E2" element={<Ejer2/>} />
-            <Route path="*" element={<NotFound/>} />
+        <Route path="/" element={<Layout />} >
+          <Route path="E1" element={<Ejer1 />} />
+          <Route path="E2" element={<Ejer2 />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="Uh20" element={<Uh20 />} />
         </Route>
       </Routes>
     </BrowserRouter>
