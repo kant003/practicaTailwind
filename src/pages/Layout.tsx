44dad9a347a6d5ea58aae8f5d6d758b020f103ed
AdrianSouto0190 <soutomoldesadrian@gmail.com>
2024-11-26 11:26:27 +0100

    commiteando cambios

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -63,6 +63,9 @@ function MainMenu() {
            <li>
                <Link to="/EjApexGraph">EjApexGraph</Link>
            </li>
+           <li>
+               <Link to="/formularioRegistro">FormularioRegistro</Link>
+           </li>
         </ul>
     </nav>
 }
