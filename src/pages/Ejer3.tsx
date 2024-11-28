5b59086eef4302c51cbf1d502e2e455a96d593da
PedroCostas <costasperezpedro@gmail.com>
2024-11-28 10:41:29 +0100

    Creacion de la pagina Con animaciones vanila

---
+++ src/pages/Ejer3.tsx
@@ -0,0 +1,19 @@
+function Ejer3 (){
+    return<div className=" px-7">
+    <div className="animate-bounce animate-infinite  ">
+    Es pagina 3 pero bota
+    </div>
+    <div className="animate-spin animate-infinite ">
+    Es pagina 3 pero gira
+    </div>
+    <div className="animate-ping animate-infinite ">
+    Es pagina 3 pero pinea
+    </div>
+    <div className="animate-pulse animate-infinite ">
+    Es pagina 3 pero es un corazon
+    </div>
+
+    </div>
+}
+
+export default Ejer3
